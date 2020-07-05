bdf89a9ec176852a784be4979f4b709477e42fa0
Mr Wilson <Cthulhoopay@users.noreply.github.com>
2020-07-05 18:58:39 +0200

    Add files via upload

---
+++ src/main/java/com/nyuulson/nyuulsoncore/NyuulsonCore.java
@@ -0,0 +1,33 @@
+package com.nyuulson.nyuulsoncore;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = NyuulsonCore.MODID, name = NyuulsonCore.NAME, version = NyuulsonCore.VERSION)
+public class NyuulsonCore
+{
+    public static final String MODID = "nyuulsoncore";
+    public static final String NAME = "Construction";
+    public static final String VERSION = "1.0";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+        
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+        
+    }
+}
