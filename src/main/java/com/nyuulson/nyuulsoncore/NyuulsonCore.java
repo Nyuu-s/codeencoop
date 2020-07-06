ecae6f10ee53dca186c6d12bd2a12a6e067376b4
Nyuu-s <cookiesarenightmares@gmail.com>
2020-07-06 23:12:18 +0200

    amethyst setup
    default item setup

--- src/main/java/com/nyuulson/nyuulsoncore/NyuulsonCore.java
+++ src/main/java/com/nyuulson/nyuulsoncore/NyuulsonCore.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.nyuulson.nyuulsoncore;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
@@ -7,11 +7,11 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = NyuulsonCore.MODID, name = NyuulsonCore.NAME, version = NyuulsonCore.VERSION)
+public class NyuulsonCore
 {
-    public static final String MODID = "examplemod";
-    public static final String NAME = "Example Mod";
+    public static final String MODID = "nyuulsoncore";
+    public static final String NAME = "Construction";
     public static final String VERSION = "1.0";
 
     private static Logger logger;
@@ -20,6 +20,7 @@ public class ExampleMod
     public void preInit(FMLPreInitializationEvent event)
     {
         logger = event.getModLog();
+        
     }
 
     @EventHandler
@@ -27,5 +28,6 @@ public class ExampleMod
     {
         // some example code
         logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+        
     }
 }
